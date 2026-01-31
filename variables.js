8391d39285260949b16524a46e394723fa6c2271
Emmanuel <emanuel.estabre@gmail.com>
2026-01-30 22:51:02 -0300

    Agrego esto para ver si lo quiero seguir en otra compu o no

---
+++ variables.js
@@ -0,0 +1,25 @@
+let numero1;
+
+numero1=10; 
+
+let numero2= 14;
+
+let numero3="Hola!!";
+
+console.log(numero1);
+
+numero3= null;
+
+let nombre = "Emmanuel";
+
+let apellido ="Estabre";
+
+console.log(nombre +" " + apellido);
+
+// Esto es una concatenacion
+
+console.log("Hola! Mi nombre es: " + nombre +" "+apellido);
+
+// Interpolacion ejemplo.
+
+console.log(`Hola! Mi nombre es:${nombre} ${apellido}`);
