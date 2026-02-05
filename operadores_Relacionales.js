9d9806501bbd8916853bae2e1df70be1f4c5d902
Emmanuel <emanuel.estabre@gmail.com>
2026-02-04 23:22:56 -0300

    Agrego archivo operadores_Relacionales

---
+++ operadores_Relacionales.js
@@ -0,0 +1,24 @@
+let resultado = 10 < 5;
+console.log(resultado);
+
+//  "+" "<>" o "=" ">=" "<="  "=="
+
+// Igualacion estricta "===" Compara valor y tipo de dato
+let resultado2= "10" === 10;
+console.log(resultado2);
+
+// No igualdad "!="
+
+let resultado3= 10 != 5;
+console.log(resultado3);
+
+// Desigualdad estricta !== Comparan valor y tipo de dato
+
+let resultado4= 10 !== "10";
+console.log(resultado4);
+
+let num1= 10;
+let num2= 5;
+
+let resultado5= num1 > num2;
+console.log(resultado5);
