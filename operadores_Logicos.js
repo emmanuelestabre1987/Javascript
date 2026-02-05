caf13992d9421301b61cda669819d09684f06801
Emmanuel <emanuel.estabre@gmail.com>
2026-02-04 23:44:58 -0300

    Agregamos operadores_Logicos

---
+++ operadores_Logicos.js
@@ -0,0 +1,15 @@
+let nota1= 7;
+let nota2= 9;
+
+let resultado;
+
+// and (&&)
+resultado = (nota1 > 6) && (nota2 > 6);
+console.log(resultado);
+
+// or (||) AltGr + 1
+resultado = (nota1 > 6) || (nota2 > 6);
+console.log(resultado);
+
+
+
