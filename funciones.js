d882b1f7ed1e6dba6373924341c261cdb350dcba
Emmanuel <emanuel.estabre@gmail.com>
2026-02-09 15:23:17 -0300

    Agrego archivo funciones

---
+++ funciones.js
@@ -0,0 +1,11 @@
+// definicion o declaracion de funcion
+
+function saludar(){
+    console.log("Hola!!");
+
+}
+
+// Invocar o llamar a la funcion
+// Realiza todo lo que esta en las llaves
+
+saludar();
