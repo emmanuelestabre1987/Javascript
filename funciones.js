10195249ebe4fc445e9e368dc6482ac736a5d162
Emmanuel <emanuel.estabre@gmail.com>
2026-02-10 04:52:49 -0300

    Actualizo funciones.js

--- funciones.js
+++ funciones.js
@@ -9,3 +9,17 @@ function saludar(){
 // Realiza todo lo que esta en las llaves
 
 saludar();
+
+// Parametros
+
+function suma(num1 , num2){
+    let resultado = num1 + num2;
+    console.log("El resultado es: "+ resultado);
+
+}
+
+// Llamar suma. En la llamada debo indicar los valores
+
+suma(10 , 5);
+suma(20,3);
+suma(100,-2);
